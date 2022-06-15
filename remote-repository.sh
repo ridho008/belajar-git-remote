514f3d1a4dc1cf2f6f67fc86cca8724913c587a5
ridho008 <ciuyy12@gmail.com>
2022-06-15 21:39:23 +0700

    remote repository

---
+++ remote-repository.sh
@@ -0,0 +1,14 @@
+
+
+# menambahkan remote repo
+# biasanya menggunakan origin
+git remote add namaremote ssh_url
+
+# melihat nama remote repository
+git remote
+
+# melihat detail remote repository
+git remote get-url namaremote
+
+# menghapus remote repository
+git remote rm namaremote
